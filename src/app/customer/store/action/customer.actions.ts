e47ac7d6e998c53de1c78c27a0af47f8d0562f16
kumarnivi <nivethikashivakumar56@gmail.com>
2023-08-23 08:43:41 +0530

    ngRx Full Basics

---
+++ src/app/customer/store/action/customer.actions.ts
@@ -0,0 +1,13 @@
+import { createAction, props } from '@ngrx/store';
+import { Customer } from 'src/app/models/customer';
+
+export const addCustomers = createAction(
+  '[Customer] Add Customers',
+    (customer:Customer)=>({customer})
+);
+
+
+
+
+
+
---
+++ src/app/customer/store/reducer/customer.reducer.ts
@@ -0,0 +1,23 @@
+import { Action, createReducer, on } from '@ngrx/store';
+import { Customer } from 'src/app/models/customer';
+import * as CustomerActions from 'src/app/customer/store/action/customer.actions';
+
+export const customerFeatureKey = 'customertest';
+
+export interface CustomerState {
+  customers:Customer[];
+}
+
+export const initialState: CustomerState = {
+  customers:[]
+};
+
+export const customerReducer = createReducer(
+  initialState,
+  on(CustomerActions.addCustomers,(state:CustomerState,{customer})=>(
+    {
+      ...state,
+      customers:[...state.customers,customer]
+    }
+  ))
+);
